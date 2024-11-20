79aea317c5643a9c28c15b466208a5e98a4658c2
Хоменко Владимир <127117566+A1exeyevich@users.noreply.github.com>
2024-11-20 11:01:35 +0900

    Zad

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Fullname { get; set; }
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string email { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person user = new Person
+            {
+                Fullname = "Хоменко",
+                Name = "Владимир",
+                Lastname = "Алексеевич",
+                email = "homenko_vova67@mail.ru",
+            };
+
+            Console.WriteLine("Имя пользователя: " + " " + user.Fullname + " " + user.Name + " " + user.Lastname);
+            Console.WriteLine($"Электронная почта: {user.email}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Square(int c)
+        {
+            return c * c;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое число для сложения:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число для сложения:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int sum = Sum(firstNumber, secondNumber);
+            Console.WriteLine($"Сумма {firstNumber} и {secondNumber} равна: {sum}");
+
+            Console.WriteLine("Введите число для вычисления квадрата:");
+            int Squarenumb = Convert.ToInt32(Console.ReadLine());
+
+            int square = Square(Squarenumb);
+            Console.WriteLine($"Квадрат числа {Squarenumb} равен: {square}");
+            Console.ReadLine();
+        }
+    }
+}
